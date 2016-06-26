5bc8a37d39f6ff4f63de0f4cb87375916af2196b
Rachel Sakry <rachelsakry@gmail.com>
2016-06-25 17:32:01 -0700

    guest name fields show'n'hide

--- public/sprinkles/invitation.js
+++ public/sprinkles/invitation.js
@@ -7,4 +7,14 @@ $(document).ready(function() {
         $('#regretsForm').addClass('hide');
         $('#yesForm').removeClass('hide');
     });
+    $('#numberOfGuests').keyup(function() {
+        var newVal = $('#numberOfGuests').val();
+        var guestInputs = $('.guest_name');
+        for (var i = newVal - 1; i >= 0; i--) {
+            $(guestInputs[i]).removeClass('hide');
+        };
+        for (var i = guestInputs.length - 1; i >= newVal; i--) {
+            $(guestInputs[i]).addClass('hide');
+        }
+    });
 });
