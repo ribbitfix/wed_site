712a5c098c235a6d9f89d832c5ba4bef20ae7d38
Rachel Sakry <rachelsakry@gmail.com>
2016-10-10 12:24:42 -0700

    disabled form submission and redirected rsvp page requests to home

--- index.js
+++ index.js
@@ -27,108 +27,110 @@ app.get('/', function(request, response) {
     });
 });
 
-app.post('/submit_invite_request', function(request, response) {
-    base('InvitationRequested').create({
-        Names: request.body.names,
-        Email: request.body.email
-    }, function(err, record) {
-        if (err) {
-            console.log('error: ', err);
-            response.render('pages/index', {
-                inviteRequestSubmitted: false,
-                submissionFailed: true,
-                home: "active",
-                location: "",
-                schedule: "",
-                gifts: ""
-            });
-        } else {
-            response.render('pages/index', {
-                inviteRequestSubmitted: true,
-                submissionFailed: false,
-                home: "active",
-                location: "",
-                schedule: "",
-                gifts: ""
-            });
-        }
-    });
-});
+// app.post('/submit_invite_request', function(request, response) {
+//     base('InvitationRequested').create({
+//         Names: request.body.names,
+//         Email: request.body.email
+//     }, function(err, record) {
+//         if (err) {
+//             console.log('error: ', err);
+//             response.render('pages/index', {
+//                 inviteRequestSubmitted: false,
+//                 submissionFailed: true,
+//                 home: "active",
+//                 location: "",
+//                 schedule: "",
+//                 gifts: ""
+//             });
+//         } else {
+//             response.render('pages/index', {
+//                 inviteRequestSubmitted: true,
+//                 submissionFailed: false,
+//                 home: "active",
+//                 location: "",
+//                 schedule: "",
+//                 gifts: ""
+//             });
+//         }
+//     });
+// });
 
-app.post('/submit_yes_rsvp', function(request, response) {
-    var carpool = request.body.Carpool === 'on' ? true : false;
-    base('YesRSVP').create({
-        NumberOfGuests: parseInt(request.body.NumberOfGuests, 10),
-        Guest1: request.body.guest1,
-        Guest2: request.body.guest2,
-        Guest3: request.body.guest3,
-        Guest4: request.body.guest4,
-        Guest5: request.body.guest5,
-        Guest6: request.body.guest6,
-        Guest7: request.body.guest7,
-        Guest8: request.body.guest8,
-        StayingAtCS: request.body.StayingAtCS,
-        InterestedInLodging: request.body.InterestedInLodging,
-        Carpool: carpool,
-        LodgingPreferences: request.body.LodgingPreferences,
-        DietInfo: request.body.DietInfo,
-        SongRequests: request.body.SongRequests
-    }, function(err, record) {
-        if (err) {
-            console.log('error: ', err);
-            response.render('pages/invitation', {
-                isDayGuest: request.body.isDayGuest,
-                submissionFailed: true
-            });
-        } else {
-            response.render('pages/index', {
-                yesRSVPsubmitted: true,
-                noRSVPsubmitted: false,
-                home: "active",
-                location: "",
-                schedule: "",
-                gifts: ""
-            });
-        }
-    });
-});
+// app.post('/submit_yes_rsvp', function(request, response) {
+//     var carpool = request.body.Carpool === 'on' ? true : false;
+//     base('YesRSVP').create({
+//         NumberOfGuests: parseInt(request.body.NumberOfGuests, 10),
+//         Guest1: request.body.guest1,
+//         Guest2: request.body.guest2,
+//         Guest3: request.body.guest3,
+//         Guest4: request.body.guest4,
+//         Guest5: request.body.guest5,
+//         Guest6: request.body.guest6,
+//         Guest7: request.body.guest7,
+//         Guest8: request.body.guest8,
+//         StayingAtCS: request.body.StayingAtCS,
+//         InterestedInLodging: request.body.InterestedInLodging,
+//         Carpool: carpool,
+//         LodgingPreferences: request.body.LodgingPreferences,
+//         DietInfo: request.body.DietInfo,
+//         SongRequests: request.body.SongRequests
+//     }, function(err, record) {
+//         if (err) {
+//             console.log('error: ', err);
+//             response.render('pages/invitation', {
+//                 isDayGuest: request.body.isDayGuest,
+//                 submissionFailed: true
+//             });
+//         } else {
+//             response.render('pages/index', {
+//                 yesRSVPsubmitted: true,
+//                 noRSVPsubmitted: false,
+//                 home: "active",
+//                 location: "",
+//                 schedule: "",
+//                 gifts: ""
+//             });
+//         }
+//     });
+// });
 
-app.post('/submit_no_rsvp', function(request, response) {
-    base('RegretsRSVP').create({
-        Name: request.body.Name,
-        Message: request.body.Message
-    }, function(err, record) {
-        if (err) {
-            console.log('error: ', err);
-            response.render('pages/invitation', {
-                isDayGuest: request.body.isDayGuest,
-                submissionFailed: true
-            });
-        } else {
-            response.render('pages/index', {
-                yesRSVPsubmitted: false,
-                noRSVPsubmitted: true,
-                home: "active",
-                location: "",
-                schedule: "",
-                gifts: ""
-            });
-        }
-    });
-});
+// app.post('/submit_no_rsvp', function(request, response) {
+//     base('RegretsRSVP').create({
+//         Name: request.body.Name,
+//         Message: request.body.Message
+//     }, function(err, record) {
+//         if (err) {
+//             console.log('error: ', err);
+//             response.render('pages/invitation', {
+//                 isDayGuest: request.body.isDayGuest,
+//                 submissionFailed: true
+//             });
+//         } else {
+//             response.render('pages/index', {
+//                 yesRSVPsubmitted: false,
+//                 noRSVPsubmitted: true,
+//                 home: "active",
+//                 location: "",
+//                 schedule: "",
+//                 gifts: ""
+//             });
+//         }
+//     });
+// });
 
 app.get('/rsvp_day', function(request, response) {
-    response.render('pages/invitation', {
-        isDayGuest: true,
-        submissionFailed: false
-    });
+    response.redirect('/');
+//     response.render('pages/invitation', {
+//         isDayGuest: true,
+//         submissionFailed: false
+//     });
 });
 
 app.get('/rsvp_overnight', function(request, response) {
-    response.render('pages/invitation', {
-        isDayGuest: false,
-        submissionFailed: false
-    });
+    response.redirect('/');
+//     response.render('pages/invitation', {
+//         isDayGuest: false,
+//         submissionFailed: false
+//     });
 });
 
 app.get('/location', function(request, response) {
